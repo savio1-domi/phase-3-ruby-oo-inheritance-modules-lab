6b2c58cda5f1599fdcf2a7ad5dbabd6f01d3d7e1
git-savio1.domi <techsavvydominic3@gmail.com>
2023-05-20 02:49:45 +0300

    Copmleted assignments

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,39 +4,41 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
+  # Class variable to store all the artist objects
   @@artists = []
 
   def initialize
-    @@artists << self
-    @songs = []
+    @@artists << self  # Add the current artist object to the @@artists array
+    @songs = []  # Initialize an empty array to store the artist's songs
   end
 
   def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
+    # Find an artist in the @@artists array based on the given name
+    @@artists.detect { |artist| artist.name == name }
   end
 
   def self.all
-    @@artists
+    @@artists  # Return the array of all artist objects
   end
 
   def self.reset_all
-    self.all.clear
+    self.all.clear  # Clear the @@artists array to reset the artist collection
   end
 
   def self.count
-    self.all.count
+    self.all.count  # Return the count of artist objects in the @@artists array
   end
 
   def add_song(song)
-    @songs << song
-    song.artist = self
+    @songs << song  # Add the song to the artist's songs array
+    song.artist = self  # Set the artist for the song
   end
 
   def add_songs(songs)
-    songs.each { |song| add_song(song) }
+    songs.each { |song| add_song(song) }  # Add multiple songs to the artist's songs array
   end
 
   def to_param
-    name.downcase.gsub(' ', '-')
+    name.downcase.gsub(' ', '-')  # Generate a URL-friendly parameter based on the artist name
   end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -4,33 +4,35 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
+  # Class variable to store all the song objects
   @@songs = []
 
   def initialize
-    @@songs << self
+    @@songs << self  # Add the current song object to the @@songs array
   end
 
   def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
+    # Find a song in the @@songs array based on the given name
+    @@songs.detect { |song| song.name == name }
   end
 
   def self.all
-    @@songs
+    @@songs  # Return the array of all song objects
   end
 
   def self.reset_all
-    self.all.clear
+    self.all.clear  # Clear the @@songs array to reset the song collection
   end
 
   def self.count
-    self.all.count
+    self.all.count  # Return the count of song objects in the @@songs array
   end
 
   def artist=(artist)
-    @artist = artist
+    @artist = artist  # Set the artist for the song
   end
 
   def to_param
-    name.downcase.gsub(' ', '-')
+    name.downcase.gsub(' ', '-')  # Generate a URL-friendly parameter based on the song name
   end
 end
